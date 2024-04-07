5b15e5fa4cc2ecc860c6a9038ac7371f0b2d5cea
Waqas Shafi <165755412+WaqasShafi194@users.noreply.github.com>
2024-04-07 00:57:14 -0800

    Add files via upload

---
+++ 12.Greeting Names/greetingnames.js
@@ -0,0 +1,6 @@
+"use strict";
+let names = ['waqas', 'shafi', 'Sir Hamzah', 'Sir Zia'];
+let message = "Welcome to typescript Class: ";
+for (let i = 0; i < names.length; i++) {
+    console.log(message + names[i]);
+}
---
+++ 12.Greeting Names/greetingnames.ts
@@ -0,0 +1,8 @@
+let names: string[] = ['waqas','shafi','Sir Hamzah','Sir Zia'];
+
+let message: string= "Welcome to typescript Class: ";
+ 
+for (let i=0 ; i<names.length ; i++) {
+    console.log(message + names[i])
+
+}
