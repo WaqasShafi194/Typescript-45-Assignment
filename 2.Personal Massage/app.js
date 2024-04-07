56330dc59a6466147de97e1f287884d0a8cba377
Waqas Shafi <165755412+WaqasShafi194@users.noreply.github.com>
2024-04-07 00:09:43 -0800

    Add files via upload

---
+++ 2.Personal Massage/app.js
@@ -0,0 +1,8 @@
+var personName = "";
+personName = prompt("What is your name?") || "";
+if (personName !== null && personName !== "") {
+    alert("Hello ".concat(personName, ",would you like to learn some python today"));
+}
+else {
+    alert("you have to fill your Name!");
+}
