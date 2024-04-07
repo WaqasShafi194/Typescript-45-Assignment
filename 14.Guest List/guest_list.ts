6e6d4ea9b0505a1e52ff51a1fbe0b0e5eb7de03b
Waqas Shafi <165755412+WaqasShafi194@users.noreply.github.com>
2024-04-07 01:00:38 -0800

    Add files via upload

---
+++ 14.Guest List/guest_list.ts
@@ -0,0 +1,6 @@
+let Guest_list: string[] = ['Imran Khan','Nawaz Sharif','Asif Ali Zardari'];
+
+for( let i=0; i<Guest_list.length; i++){
+    
+    console.log('Dear Mr. ' + Guest_list[i] + ',\n\nIt is our pleasure to invite you in our party.\n\nThank you\n')
+}
