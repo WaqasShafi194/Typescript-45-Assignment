765f96362f82cc8d3b074ce79f2d7771ca5de7f5
Waqas Shafi <165755412+WaqasShafi194@users.noreply.github.com>
2024-04-07 00:59:04 -0800

    Add files via upload

---
+++ 13.Your own Arey/your_own_arey.js
@@ -0,0 +1,5 @@
+"use strict";
+let Transportation = ['Motorcycle', 'Car', 'Train', 'Cycle'];
+for (let i = 0; i < Transportation.length; i++) {
+    console.log("I would like to own a " + Transportation[i]);
+}
---
+++ 13.Your own Arey/your_own_arey.ts
@@ -0,0 +1,6 @@
+let Transportation: string[] = ['Motorcycle','Car','Train','Cycle'] ;
+
+for(let i=0; i<Transportation.length; i++){
+    console.log("I would like to own a " + Transportation[i])
+}
+
