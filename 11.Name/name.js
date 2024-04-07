1ce09c36a86b142bdd145e44252d508b5f07b8e9
Waqas Shafi <165755412+WaqasShafi194@users.noreply.github.com>
2024-04-07 00:55:47 -0800

    Add files via upload

---
+++ 11.Name/name.js
@@ -0,0 +1,5 @@
+"use strict";
+let names = ['waqas', 'shafi', 'Sir Hamzah', 'Sir Zia'];
+for (let i = 0; i < names.length; i++) {
+    console.log(names[i]);
+}
---
+++ 11.Name/name.ts
@@ -0,0 +1,6 @@
+let names: string[] = ['waqas','shafi','Sir Hamzah','Sir Zia'];
+ 
+for (let i=0 ; i<names.length ; i++) {
+    console.log(names[i])
+
+}
